dc29661428979eab00b3a9e5177c21e8c34e8b32
elcoosp <elcoosp@gmail.com>
2025-12-17 16:19:14 +0100

    fix: lockfile & rm auth from shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,15 +1,2 @@
 // Export all types
-export * from './types/auth';
 export * from './types/garden';
-
-// Export DTOs
-export interface RegisterDto {
-  email: string;
-  password: string;
-  name: string;
-}
-
-export interface LoginDto {
-  email: string;
-  password: string;
-}
